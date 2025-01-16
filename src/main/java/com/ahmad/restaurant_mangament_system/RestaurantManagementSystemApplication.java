a4fe3aa0b3f36d962a6e70597bdc44ccd5d091f6
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-01-16 12:56:51 +0200

    init project

---
+++ src/main/java/com/ahmad/restaurant_mangament_system/RestaurantManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ahmad.restaurant_mangament_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmad/restaurant_mangament_system/RestaurantMangamentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmad.restaurant_mangament_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMangamentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

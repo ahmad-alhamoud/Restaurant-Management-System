d0d4309b8dd1361e5fbbd8e21d0b103c09e985cc
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-01-27 17:31:25 +0200

    solved Global Exception Handler

--- src/main/java/com/ahmad/restaurant_mangament_system/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ahmad/restaurant_mangament_system/exception/GlobalExceptionHandler.java
@@ -1,13 +1,12 @@
 package com.ahmad.restaurant_mangament_system.exception;
 
+import com.ahmad.restaurant_mangament_system.security.dto.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
@@ -23,27 +22,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public ErrorResponse handleNotFoundException(NotFoundException e) {
+    public ApiErrorResponse handleNotFoundException(NotFoundException e) {
    //     log.error(e.getMessage());
-        return new ErrorResponse(404, e.getMessage());
+        return new ApiErrorResponse(404, e.getMessage());
     }
 
-    @ExceptionHandler(value = HandlerException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ErrorResponse handlerException(HandlerException e) {
+    @ExceptionHandler(value = HandlerException.class)
+    public ApiErrorResponse handlerException(HandlerException e) {
       //  log.error(e.getMessage());
-        return new ErrorResponse(400, e.getMessage());
+        return new ApiErrorResponse(400, e.getMessage());
     }
 
-
-    static class ErrorResponse {
-        private Integer code;
-        private String message;
-
-        ErrorResponse(Integer code, String message) {
-            this.code = code;
-            this.message = message;
-        }
-    }
 }
